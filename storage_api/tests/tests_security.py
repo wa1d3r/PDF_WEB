69dc8069be41d38c507319c7fc8e95a4b5a571af
wa1d3r <yaremko2021@mail.ru>
2026-04-29 14:11:53 +0300

    update tests

--- storage_api/tests/tests_security.py
+++ storage_api/tests/tests_security.py
@@ -28,7 +28,7 @@ def test_verify_success(mock_dns, nac):
 
 def test_verify_invalid_format(nac):
     token = 'invalid'
-    assert nac.verify_access(token, client_ip='172.18.0.5') id False
+    assert nac.verify_access(token, client_ip='172.18.0.5') is False
 
 @patch('socket.gethostbyname')
 def test_verify_invalid_ip(mock_dns, nac):
@@ -40,4 +40,24 @@ def test_verify_invalid_ip(mock_dns, nac):
 
 def test_verify_invalid_signature(nac):
     token = 'test-domain-1.invalid'
-    assert nac.verify_access(token, client_ip='172.18.0.5') id False
+    assert nac.verify_access(token, client_ip='172.18.0.5') is False
+
+def test_verify_empty_none_token(nac):
+    assert nac.verify_access(None, client_ip='172.18.0.5') is False
+    assert nac.verify_access('', client_ip='172.18.0.5') is False
+
+@patch('socket.gethostbyname')
+def test_verify_unallowed_domain(nac):
+    domain_ip = '172.18.0.5'
+    invalid_domain = 'invalid-domain'
+
+    mock_dns.return_value = domain_ip
+    signature = nac._sign_payload(invalid_domain)
+    token = f"{invalid_domain}.{signature}"
+    assert nac.verify_access(token, client_ip=domain_ip) is False
+
+@patch('socket.gethostbyname')
+def test_verify_dns_failed(mock_dns, nac):
+    mock_dns.side_effect = socket.gaierror()
+    token = nac.generate_token('test-domain-1')
+    assert nac.verify_access(token, client_ip='172.18.0.5') is False
