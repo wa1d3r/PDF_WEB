2f1a19855f4511b8598337d47c22e29d40630682
wa1d3r <yaremko2021@mail.ru>
2026-04-29 20:27:55 +0300

    add redis module

---
+++ storage_api/src/db/redis.py
@@ -0,0 +1,12 @@
+import redis.asyncio as redis
+from src.core.config import settings
+
+redis_client = redis.from_url(settings.REDIS_URL, decode_responses=True)
+
+async def get_redis() -> redis.Redis:
+    """Зависимость для получения БД
+
+    Returns:
+        redis.Redis: БД из глобального пула.
+    """
+    return redis_client
