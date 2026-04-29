63126571402fd1c77197b1c850abf155200ddeb6
wa1d3r <yaremko2021@mail.ru>
2026-04-29 21:06:55 +0300

    add routes deps (internal routes)

---
+++ storage_api/src/api/deps.py
@@ -0,0 +1,41 @@
+from typing import Annotated
+from fastapi import Request, Header, HTTPException
+from src.core.security import NetworkAccessControl
+from src.core.config import settings
+
+nac = NetworkAccessControl(
+    master_secret=settings.MASTER_SECRET,
+    allowed_domains=settings.ALLOWED_DOMAINS
+)
+
+async def verify_service_access(
+        request: Request,
+        x_service_token: Annotated[
+            str,
+            Header(
+                alias="X-Service-Token",
+                description='Токен доступа в формате \'<домен>.<подпись>\''
+            )
+        ]
+) -> str:
+    """Зависимость для защиты приватных эндпоинтов.
+
+    Args:
+        request (Request): Объект запроса FastAPI.
+        x_service_token (str): Токен, переданный в заголовке X-Service-Token.
+    
+    Raises:
+        HTTPException: Ошибка 401, если токен не прошел валидацию
+
+    Returns:
+        str: Валидированный токен сервиса
+    """
+    client_ip = request.client.host if request.client else 'unknown'
+
+    if not nac.verify_access(x_service_token, client_ip):
+        raise HTTPException(
+            status_code=401,
+            detail='Unauthorized. Invalid token.'
+        )
+
+    return x_service_token
