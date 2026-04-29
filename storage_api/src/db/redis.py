24e76260327674702e0f368f1a33e625407e7f1f
wa1d3r <yaremko2021@mail.ru>
2026-04-29 21:18:33 +0300

    redis return bytes

--- storage_api/src/db/redis.py
+++ storage_api/src/db/redis.py
@@ -1,7 +1,7 @@
 import redis.asyncio as redis
 from src.core.config import settings
 
-redis_client = redis.from_url(settings.REDIS_URL, decode_responses=True)
+redis_client = redis.from_url(settings.REDIS_URL)
 
 async def get_redis() -> redis.Redis:
     """Зависимость для получения БД
