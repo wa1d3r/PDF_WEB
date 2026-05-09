c73ecd48edacd6bcf7297da55d652cffd6d6a7d2
wa1d3r <yaremko2021@mail.ru>
2026-05-09 20:10:41 +0300

    add config

---
+++ PDF_WEB/pdf_gen/src/core/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    """Класс настроек сервиса PDF Gen.
+    """
+    PROJECT_NAME: str = "PDF Generator Node"
+    STORAGE_API_URL: str = "http://storage"
+    SIGNER_API_URL: str = "http://signer"
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+settings = Settings()
