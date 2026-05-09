358ac3dbeb329b1dfa353281c07212cceac84633
wa1d3r <yaremko2021@mail.ru>
2026-05-09 20:37:36 +0300

    add main

---
+++ PDF_WEB/pdf_gen/src/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from src.core.config import settings
+from src.api import generate
+
+app = FastAPI(title=settings.PROJECT_NAME)
+
+app.include_router(generate.router)
+
+@app.get('/health', tags=['System'], summary='Проверка статуса сервиса.')
+async def health_check() -> dict[str, str]:
+    """Базовый Health-check
+    
+    Returns:
+        dict[str, str]: Статус работоспособности сервера
+    """
+    return {'status': 'ok'}
