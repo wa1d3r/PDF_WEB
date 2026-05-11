aceaf43d9079d45f84e9882471f0a9a7643d9778
wa1d3r <yaremko2021@mail.ru>
2026-05-11 11:47:10 +0300

    work with json

--- storage_api/src/core/config.py
+++ storage_api/src/core/config.py
@@ -5,12 +5,12 @@ class Settings(BaseSettings):
 
     Attributes:
         PROJECT_NAME (str): Название проекта.
-        REDIS_URL (str): Адрес сервера Redis.
+        STORAGE_FILE_PATH (str): Путь к JSON файлу с данными хранилища.
         MASTER_SECRET (str): Ключ для генерации и валидации HMAC токенов.
         ALLOWED_DOMAINS (list[str]): Список доменов, имеющих доступ к закрытому API
     """
     PROJECT_NAME: str = "Storage API"
-    REDIS_URL: str = "redis://redis:6379/0"
+    STORAGE_FILE_PATH: str = "data.json"
     MASTER_SECRET: str = "secret-key"
     ALLOWED_DOMAINS: list[str] = []
 
