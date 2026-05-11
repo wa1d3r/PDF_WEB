784a6c1be52b7d5993cd6d775af6f670d4b81433
wa1d3r <yaremko2021@mail.ru>
2026-05-11 19:15:01 +0300

    add clients logging

--- pdf_gen/src/services/clients.py
+++ pdf_gen/src/services/clients.py
@@ -1,13 +1,16 @@
 import httpx
+import logging
 import base64
 from src.core.config import settings
 from src.core.exceptions import NetworkError
+
+logger = logging.getLogger(__name__)
 
 class StorageClient:
     """Клиент для получения публичных ассетов из Storage API."""
     
     async def get_template(self, template_name: str) -> str:
-        """Скачивает и декодирует HTML-шаблон из хранилища.
+        """Скачивает и декодирует шаблон из хранилища.
 
         Args:
             template_name (str): Имя файла шаблона в хранилище.
@@ -16,8 +19,9 @@ class StorageClient:
             NetworkError: Ошибка при обращении к хранилищу
 
         Returns:
-            str: Декодированный HTML код шаблона.
+            str: Декодированный код шаблона.
         """
+        logger.info(f"Fetching template '{template_name}' from Storage API...")
         url = f"{settings.STORAGE_API_URL}/public/assets/{template_name}"
         
         try:
@@ -27,9 +31,12 @@ class StorageClient:
                 
                 b64_data = response.json().get("data", "")
                 decoded_bytes = base64.b64decode(b64_data)
+
+                logger.info(f"Successfully fetched and decoded template '{template_name}'.")
                 return decoded_bytes.decode('utf-8')
                 
         except Exception as e:
+            logger.error(f"Failed to fetch template from Storage API: {e}")
             raise NetworkError(f"Storage API error: {str(e)}")
 
 class SignerClient:
@@ -48,6 +55,7 @@ class SignerClient:
             str: Подписанный PDF документ в Base64.
         """
         url = f"{settings.SIGNER_API_URL}/api/v1/sign"
+        logger.info("Sending rendered PDF to Signer API for cryptographic signature...")
         
         payload = {
             "document_base64": pdf_base64,
@@ -59,7 +67,10 @@ class SignerClient:
             async with httpx.AsyncClient() as client:
                 response = await client.post(url, json=payload, timeout=10.0)
                 response.raise_for_status()
+
+                logger.info("Successfully received signed PDF from Signer API.")
                 return response.json().get("signed_document_base64", "")
                 
         except Exception as e:
+            logger.error(f"Failed to sign PDF via Signer API: {e}")
             raise NetworkError(f"Signer API error: {str(e)}")
