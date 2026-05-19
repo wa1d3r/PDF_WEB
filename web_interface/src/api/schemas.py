7671105d920a8717adacb343cd73043fbc7189b2
wa1d3r <96950643+wa1d3r@users.noreply.github.com>
2026-05-19 16:04:26 +0300

    add task schemas

--- web_interface/src/api/schemas.py
+++ web_interface/src/api/schemas.py
@@ -1,6 +1,19 @@
 from pydantic import BaseModel, Field
+from typing import List, Optional
 
 class TokenRequest(BaseModel):
     """Модель запроса токена игрока
     """
     token: str = Field(..., description='Токен участника CTFd')
+
+class TaskData(BaseModel):
+    title: str
+    score: int
+    attempts: int
+    comment: Optional[str] = None
+
+class ManualReportRequest(BaseModel):
+    username: str
+    team: str
+    score: int
+    tasks: List[TaskData]
